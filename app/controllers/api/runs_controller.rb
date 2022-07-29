55039e60e30c16ae3723e6de454188367823d8e0
Aaron Briggs <aaron00.briggs@gmail.com>
2022-07-28 22:42:04 -0500

    initial CRUD controller actions

--- app/controllers/api/runs_controller.rb
+++ app/controllers/api/runs_controller.rb
@@ -1,21 +1,46 @@
 class Api::RunsController < ApplicationController
+
+    skip_before_action :verify_authenticity_token
+
     def index
-        render "index"
+        @runs = Run.all
+        render json: { runs: @runs }, status: :ok
     end
     
     def show
-        render 'show'     
+        @run = Run.find(params[:id])
+        render json: { run: @run }, status: :ok    
     end
 
     def create
-        render 'create'  
+        @run = Run.new(
+            line: params[:line],
+            route: params[:route],
+            run_number: params[:run_number],
+            operator_id: params[:operator_id]
+        )
+        if @run.save
+            render json: { run: @run }, status: :ok   
+        else
+            render json: { errors: @run.errors.full_messages }, status: :bad_request
+        end  
     end
 
-    def edit
-        render 'edit'
+    def update
+        @run = Run.find(params[:id])
+        @run.line = params[:line] || @run.line
+        @run.route = params[:route] || @run.route
+        @run.run_number = params[:run_number] || @run.run_number
+        @run.operator_id = params[:operator_id] || @run.operator_id
+        if @run.save
+            render json: { run: @run }, status: :ok   
+        else
+            render json: { errors: @run.errors.full_messages }, status: :bad_request
+        end  
     end
 
-    def delete
-        render 'delete'
+    def destroy
+        Run.find(params[:id]).destroy
+        render json: { message: 'run successfully destroyed' }, status: :ok
     end
 end
--- app/models/run.rb
+++ app/models/run.rb
@@ -1,2 +1,3 @@
 class Run < ApplicationRecord
+    validates :line, :route, :run_number, :operator_id, :presence => true
 end
