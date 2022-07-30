4d17ce285872b8b8516eb26bbbfb585d105c8bfe
Aaron Briggs <aaron00.briggs@gmail.com>
2022-07-29 20:41:43 -0500

    edits cors permission

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://trains-react-app-i-guess.netlify.app'
+      origins 'https://main--trains-react-app-i-guess.netlify.app/'
   
       resource '*',
         headers: :any,
